5023ea798905ae165be3836ff4ff8c9949a08532
Hasitha Sanjaya <79785680+hasitha1998@users.noreply.github.com>
2023-10-20 15:42:19 +0530

    update base url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target: 'https://weather-azure-app.azurewebsites.net',
+        target: 'https://weather-azure-app.azurewebsites.net/',
         changeOrigin: true
       }
     }
